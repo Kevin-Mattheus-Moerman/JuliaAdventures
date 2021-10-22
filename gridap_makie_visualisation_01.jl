30f124aa7cca734869476e74785e5d569a68793a
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2021-10-22 20:04:05 +0100

    Added slider to gridap test

--- gridap_makie_visualisation_01.jl
+++ gridap_makie_visualisation_01.jl
@@ -11,33 +11,33 @@ using FileIO
 using LineSearches: BackTracking
 using GLMakie, GeometryBasics
 
+# Geometry and BC parameters
+sample_dim = [1,1,1] #Sample dimensions
+numElem    = [8,8,8] #Number of elements in each direction
+disp_max   = 0.8 #Maximum displacement
+disp_inc   = 0.1 #Desired displacement increment per step
+degree     = 2 #Mesh order
+
 # Material parameters
 const λ = 100.0
 const μ = 1.0
 
-# BC parameters
-disp_max = 0.8
-disp_inc = 0.1
-
 # Deformation Gradient
 F(∇u) = one(∇u) + ∇u'
 
-J(F) = sqrt(det(C(F)))
+#Jacobian = volume ratio
+J(F) = sqrt(det(C(F))) 
 
-#Green strain
-
-#E(F) = 0.5*( F'*F - one(F) )
-
+#Green-Lagrange strain
+#E(F) = 0.5*( F'*F - one(F) ) #Green-Lagrange strain
 dE(∇du,∇u) = 0.5*( ∇du⋅F(∇u) + (∇du⋅F(∇u))' )
 
 # Right Cauchy-green deformation tensor
-
 C(F) = (F')⋅F
 
-# Constitutive law (Neo hookean)
-
+# Hyperelastic constitutive law for the Neo hookean material
 function S(∇u)
-  Cinv = inv(C(F(∇u)))
+  Cinv = inv(C(F(∇u))) #Inverse of C i.e. B
   μ*(one(∇u)-Cinv) + λ*log(J(F(∇u)))*Cinv
 end
 
@@ -48,13 +48,11 @@ function dS(∇du,∇u)
 end
 
 # Cauchy stress tensor
-
 σ(∇u) = (1.0/J(F(∇u)))*F(∇u)⋅S(∇u)⋅(F(∇u))'
 
 # Model
-nElem=8
-domain = (0,1,0,1,0,1)
-partition = (nElem,nElem,nElem)
+domain = (0,sample_dim[1],0,sample_dim[2],0,sample_dim[3])
+partition = (numElem[1],numElem[2],numElem[3])
 model = CartesianDiscreteModel(domain,partition)
 
 # Define new boundaries
@@ -63,30 +61,24 @@ add_tag_from_tags!(labels,"diri_0",[1,3,5,7,13,15,17,19,25])
 add_tag_from_tags!(labels,"diri_1",[2,4,6,8,14,16,18,20,26])
 
 # Setup integration
-degree = 2
 Ω = Triangulation(model)
 dΩ = Measure(Ω,degree)
 
 # Weak form
-
 res(u,v) = ∫( (dE∘(∇(v),∇(u))) ⊙ (S∘∇(u)) )*dΩ
 
 jac_mat(u,du,v) =  ∫( (dE∘(∇(v),∇(u))) ⊙ (dS∘(∇(du),∇(u))) )*dΩ
 
 jac_geo(u,du,v) = ∫( ∇(v) ⊙ ( (S∘∇(u))⋅∇(du) ) )*dΩ
 
-jac(u,du,v) = jac_mat(u,v,du) + jac_geo(u,v,du)
+jac(u,du,v) = jac_mat(u,du,v) + jac_geo(u,du,v)
 
 # Construct the FEspace
 reffe = ReferenceFE(lagrangian,VectorValue{3,Float64},1)
 V = TestFESpace(model,reffe,conformity=:H1,dirichlet_tags = ["diri_0", "diri_1"])
 
 # Setup non-linear solver
-nls = NLSolver(
-  show_trace=true,
-  method=:newton,
-  linesearch=BackTracking())
-
+nls = NLSolver(show_trace=true,method=:newton,linesearch=BackTracking())
 solver = FESolver(nls)
 
 function run(x0,disp_x,step,nsteps,cache)
@@ -104,8 +96,6 @@ function run(x0,disp_x,step,nsteps,cache)
 
   uh, cache = solve!(uh,solver,op,cache)
 
-  #writevtk(Ω,"results_$(lpad(step,3,'0'))",cellfields=["uh"=>uh,"sigma"=>σ∘∇(uh)])
-
   return get_free_dof_values(uh), uh, cache
 
 end
@@ -115,7 +105,7 @@ function runs(disp_max,disp_inc)
  nsteps = ceil(Int,abs(disp_max)/disp_inc)
 
  x0 = zeros(Float64,num_free_dofs(V))
- nodalDisplacements = Vector{Vector{VectorValue{3, Float64}}}(undef,nsteps)
+ nodalDisplacements = Vector{Vector{VectorValue{3, Float64}}}(undef,nsteps+1)
 
  cache = nothing
  for step in 1:nsteps
@@ -123,9 +113,11 @@ function runs(disp_max,disp_inc)
   x0, uh, cache = run(x0,disp_x,step,nsteps,cache)
 
   vd = visualization_data(Ω,"",cellfields=["u"=>uh])
-  nodalDisplacements[step] = vd[1].nodaldata["u"]
+  nodalDisplacements[step+1] = vd[1].nodaldata["u"]
  end
  
+ nodalDisplacements[1]=nodalDisplacements[2].*0 #Add zeros for initial state
+
  return nodalDisplacements
 end
 
@@ -220,11 +212,11 @@ M = lift(sl_step.value) do stepIndex
 end
 
 titleString = lift(sl_step.value) do stepIndex
-  "Step: "*string(stepIndex)
+  "Step: "*string(stepIndex-1)
 end
 
 ax=Axis3(fig[1, 1], aspect = :data, xlabel = "X", ylabel = "Y", zlabel = "Z", title = titleString)
-poly!(M, strokewidth=2,shading=true,color=nodalColor, transparency=false, overdraw=false)
+hp=poly!(M, strokewidth=3,shading=true,color=nodalColor, transparency=false, overdraw=false
+,colormap = Reverse(:Spectral),colorrange=(0,0.8))
+Colorbar(fig[1, 2],hp.plots[1],label = "Displacement magnitude [mm]")
 fig
-
-# Colorbar(fig[1, 2], limits = (0, 10), colormap = :viridis,vertical = true)
--- temp_slider_test_01.jl
+++ temp_slider_test_01.jl
@@ -1,18 +1,23 @@
 
-using GLMakie, GeometryBasics
+using GLMakie
+
+xs = range(0, 10, length = 25)
+ys = range(0, 15, length = 25)
+
 fig = Figure()
 
-ax = Axis(fig[1, 1])
+sl = Slider(fig[2, 1], range = 0:0.1:3, startvalue = 1)
 
-sl_x = Slider(fig[2, 1], range = 0:0.01:10, startvalue = 3)
-sl_y = Slider(fig[1, 2], range = 0:0.01:10, horizontal = false, startvalue = 6)
-
-point = lift(sl_x.value, sl_y.value) do x, y
-    convert(GeometryBasics.Point,(x, y))
+zs = lift(sl.value) do s
+    convert(Float64,s).*[cos(x) * sin(y) for x in xs, y in ys]
 end
 
-scatter!(point, color = :red, markersize = 20)
+titleString = lift(sl.value) do s
+    "Step: "*string(s)
+end
 
-limits!(ax, 0, 10, 0, 10)
+ax = Axis(fig[1, 1], title = titleString)
 
+hm=heatmap!(xs, ys, zs,colormap = Reverse(:Spectral))
+Colorbar(fig[1, 2],hm,label = "Values")
 fig
