cd74a2258011b5975a33ec761b180aa0288fc2bc
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2023-06-12 11:03:52 +0100

    Updated slider test code

--- temp_slider_test_01.jl
+++ temp_slider_test_01.jl
@@ -1,15 +1,15 @@
 
 using GLMakie
 
-xs = range(0, 10, length = 25)
-ys = range(0, 15, length = 25)
+xs = range(0, 10*pi, length = 150)
+ys = range(0, 10*pi, length = 150)
 
 fig = Figure()
 
 sl = Slider(fig[2, 1], range = 0:0.1:3, startvalue = 1)
 
 zs = lift(sl.value) do s
-    convert(Float64,s).*[cos(x) * sin(y) for x in xs, y in ys]
+    convert(Float64,s).*[cos(x*s) * sin(y*s) for x in xs, y in ys]
 end
 
 titleString = lift(sl.value) do s
